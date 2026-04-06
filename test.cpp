563e334f4fc55fad5a03c773aaa145b41649fe8f
Johan Emmanuelli <johan.emmanuelli2004@gmail.com>
2026-04-06 21:45:11 +0200

    feat: demo window, yeaaaah

--- test.cpp
+++ test.cpp
@@ -10,8 +10,14 @@
 
 #include "schematica/parser.hh"
 
-int main()
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "GLFW/glfw3.h"
+
+int _main()
 {
+
   std::cout << "Rendering scene..." << std::endl;
   size_t terrain_size = 500;
   double scale = 4.0 / terrain_size;
@@ -49,6 +55,114 @@ int main()
       image[i].save(filename.c_str());
       std::cout << "Scene rendered and saved to " << filename << std::endl;
     }
+
+  return 0;
+}
+
+int main()
+{
+  if (!glfwInit())
+    return -1;
+  
+  const char* glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+
+  // Create window with graphics context
+  float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
+  GLFWwindow* window = glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale), "Minecraft", nullptr, nullptr);
+
+  if (window == nullptr)
+      return 1;
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // Enable vsync
+
+  
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+  ImGui::StyleColorsDark();
+  
+  // Setup scaling
+  ImGuiStyle& style = ImGui::GetStyle();
+  style.ScaleAllSizes(main_scale);
+  style.FontScaleDpi = main_scale;
+
+  // Setup backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version); // macOS = 150
+
+  
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  char buf[250] = { 0 };
+  float f;
+
+  // Main loop
+  while (!glfwWindowShouldClose(window))
+  {
+      glfwPollEvents();
+
+      // New frame
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+
+      
+      // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+      if (show_demo_window)
+          ImGui::ShowDemoWindow(&show_demo_window);
+
+    
+      // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+      {
+          static float f = 0.0f;
+          static int counter = 0;
+
+          ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+          ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+          ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+          ImGui::Checkbox("Another Window", &show_another_window);
+
+          ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+          ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+          if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+              counter++;
+          ImGui::SameLine();
+          ImGui::Text("counter = %d", counter);
+
+          ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+          ImGui::End();
+      }
+
+      // Render
+
+      ImGui::Render();
+      int display_w, display_h;
+      glfwGetFramebufferSize(window, &display_w, &display_h);
+      glViewport(0, 0, display_w, display_h);
+      glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+      glClear(GL_COLOR_BUFFER_BIT);
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+      glfwSwapBuffers(window);
+  }
+
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   return 0;
 }
